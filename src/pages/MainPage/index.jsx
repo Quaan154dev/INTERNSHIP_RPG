27507d08c012ac3029b04a108dfa5c79cd00dfcd
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-12 14:32:25 +0700

    Revert "setup project"
    This reverts commit 0f0a65dd7e95bc513ac83ed7486775c83a2500d3.

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import NotFound from "../NotFound";
+import Home from "../Home";
 
 function MainPage() {
-  return <div>index</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound></NotFound>} />
+    </Routes>
+  );
 }
 
 export default MainPage;
