038cabaa944c9a76a6a7156ed9d2a4df4bf43be2
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-06-19 19:26:59 +0700

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,23 +4,26 @@ import Navbar from "../../components/Navbar";
 import { StarsCanvas } from "../../components/canvas";
 import HomeBanner from "../../components/HomeBanner";
 import { useTheme } from "../../ThemeContext";
+import Choice from "../../components/Choice";
 
 function Home() {
   const { season } = useTheme();
 
   return (
     <>
-      <div className={`${season}-gradient relative z-0 h-screen `}>
+      <div className={`${season}-gradient  z-0 h-screen relative `}>
         <StarsCanvas />
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar active="home" />
-          <div className="relative flex justify-center">
-            <div className="flex flex-row h-svh justify-around items-center text-center w-full relative max-md:flex-col ">
+
+          <div className=" flex justify-center">
+            <div className="flex flex-row h-svh justify-around items-center text-center w-full  max-md:flex-col ">
               <TextBanner />
 
               <HomeBanner />
             </div>
           </div>
+          <Choice feedbackCount={26} visited={43} />
         </div>
       </div>
     </>
