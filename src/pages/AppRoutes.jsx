3acddeadc7c37eebe7db7c50763cdb7b4c1b431c
Đào Bá Anh Quân <quandba154@gmail.com>
2024-08-28 07:16:58 +0700

    Initial commit

--- src/pages/AppRoutes.jsx
+++ src/pages/AppRoutes.jsx
@@ -6,7 +6,7 @@ import Home from "./Home";
 import Scene from "./SadStory";
 import SadStory from "./SadStory/trailer";
 import PlayHard from "./PlayHard";
-
+import Trailer from "./WordGame/trailer";
 function AppRoutes() {
   return (
     <Routes>
@@ -14,6 +14,10 @@ function AppRoutes() {
       <Route path="/home" element={<Home></Home>} />
       <Route path="/play" element={<PlayHard></PlayHard>} />
       <Route path="/sadstory" element={<SadStory></SadStory>} />
+
+      <Route path="/trailer" element={<Scene></Scene>} />
+      <Route path="/trailer2" element={<Trailer></Trailer>} />
+
       <Route path="404" element={<NotFound />} />
     </Routes>
   );
